b779d4acc8919ab17cb77294ea987fa57d060433
Steve Lipton <stevelipton@brk-16892.local>
2018-02-27 08:26:56 -0800

    initial commit

--- HexColorComplement/ViewController.swift
+++ HexColorComplement/ViewController.swift
@@ -8,18 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController:UIViewController{
+    
+    @IBOutlet weak var topButton: UIButton!
+    @IBOutlet weak var bottomButton: UIButton!
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    //MARK: - Actions
+    @IBAction func goButton(_ sender: UIButton) {
+        textField.resignFirstResponder()
+        updateColor(colorString: textField.text!)
+    }
+    
+    @IBAction func newColor(_ sender: UIButton) {
+        textField.becomeFirstResponder()
+    }
+    
+    func color(from hexColorNumber:UInt32) -> UIColor{
+        let blue = hexColorNumber & 0x0000ff
+        let green = (hexColorNumber & 0x00ff00) >> 8
+        let red = (hexColorNumber & 0xff0000) >> 16
+        return UIColor(red: CGFloat(red) / 255.0 , green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    func color(from string:String) -> UIColor{
+        if let colorNumber = UInt32(string, radix:16){
+            return color(from: colorNumber)
+        } else {
+            return UIColor.black
+        }
+    }
+    
+    func colorComplement(from string:String) -> UIColor{
+        if let colorNumber = UInt32(string, radix:16){
+            return color(from: ~colorNumber)
+        } else {
+            return UIColor.black
+        }
+    }
+    func updateColor(colorString:String){
+        topButton.backgroundColor = color(from: colorString)
+        topButton.tintColor = colorComplement(from: colorString)
+        bottomButton.backgroundColor = colorComplement(from: colorString)
+        bottomButton.tintColor = color(from: colorString)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateColor(colorString:"ffff00")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
